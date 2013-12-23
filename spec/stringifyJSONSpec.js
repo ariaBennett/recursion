3cc1c294ff1c1dcd705f327440e951f7aa9581f6
AriaB <ariasayshi@gmail.com>
2013-12-22 16:17:33 -0800

    up to arrays with contents on stringify

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,7 +1,50 @@
+function stringifyJSON(val) {
+  debugMode = true;
+  if (debugMode) {console.log("val == " + val);}
+
+  if (typeof(val) == "number") {
+    return val.toString();
+  }
+
+  if (val == null) {
+    return "null";
+  }
+
+  if (typeof(val) == "boolean") {
+    if (val) {
+      return 'true';
+    }
+    else {
+      return 'false';
+    }
+  }
+
+  if (typeof(val) == "string") {
+    return '"' + val + '"';
+  }
+
+  if (typeof(val) == "object") {
+    var type = Object.prototype.toString.call(val);
+    if (type == "[object Array]") {
+      var items = "";
+      _.each(val, function(item){
+          items = items + stringifyJSON(item);
+      });
+      return "[" + items + "]";
+    }
+  }
+
+  if (debugMode) {console.log("val == " + val);}
+
+  return val;
+
+}
+
 // test cases are described in fixtures.js
 describe("stringifyJSON", function(){
   it("should match the result of calling JSON.stringify", function(){
 
+    arrayWithValidElements = stringifiableValues;
     arrayWithValidElements.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
