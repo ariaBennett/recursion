0ab4b6ac48de853a3d65ee6a646582199f3b32e5
Aria <aria@aria-VirtualBox.(none)>
2013-12-26 18:03:28 -0800

    getElementsByClassName almost working.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,40 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  var objects = document.body.children;
+  var matches = [];
+  all = [];
+
+  var findClass = function(object) {
+    _.each(object.classList, function(potentialClass){
+      console.log("potentialClass: " + potentialClass);
+      if (className == potentialClass) {
+        console.log("found it: " + object);
+        matches.push(object);
+      }
+    });
+  };
+
+  var findSubobjects = function(object) {
+    try {
+    all.push(object);
+      _.each(object, function(subObj){
+        if (subObj.hasChildNodes) {
+          console.log("subObj: " + subObj);
+          findSubobjects(sobObj);
+        }
+      });
+    }
+    catch(err) {
+    all.push(object);
+      console.log("hit bottom");
+      findClass(object);
+    }
+  };
+
+  _.each(objects, function(object){
+    findSubobjects(object);
+  });
+
+  return matches;
 };
